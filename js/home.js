3ecf219a74c0deb0cee26da8cb9141ef1defebd3
Harsh Malik <saabharsh78@gmail.com>
2024-07-11 13:59:33 +0530

    done

--- js/home.js
+++ js/home.js
@@ -1,6 +1,39 @@
+document.getElementById("digital_marketing").style.display = "none";
+document.getElementById("graphic_designing").style.display = "none";
+document.getElementById("website_list").style.backgroundColor="#5e45f8"
 
 
+// WEBISTE 
+document.getElementById("website_list").addEventListener("click", () => {
+  document.getElementById("website").style.display = "flex";
+  document.getElementById("website").set
+  document.getElementById("website_list").style.backgroundColor="#5e45f8"
+  document.getElementById("digital_marketing_list").style.backgroundColor="transparent"
+  document.getElementById("graphic_designing_list").style.backgroundColor="transparent"
+  document.getElementById("digital_marketing").style.display = "none";
+  document.getElementById("graphic_designing").style.display = "none";
+});
 
+// DIGITAL MARKETING 
+document
+  .getElementById("digital_marketing_list")
+  .addEventListener("click", () => {
+  document.getElementById("website_list").style.backgroundColor="transparent";
+  document.getElementById("digital_marketing_list").style.backgroundColor="#5e45f8";
+  document.getElementById("graphic_designing_list").style.backgroundColor="transparent";
+    document.getElementById("website").style.display = "none";
+    document.getElementById("digital_marketing").style.display = "flex";
+    document.getElementById("graphic_designing").style.display = "none";
+  });
 
-
-
+//   GRAPHIC DESIGNING
+document
+  .getElementById("graphic_designing_list")
+  .addEventListener("click", () => {
+    document.getElementById("website_list").style.backgroundColor="transparent";
+  document.getElementById("digital_marketing_list").style.backgroundColor="transparent";
+  document.getElementById("graphic_designing_list").style.backgroundColor="#5e45f8";
+    document.getElementById("website").style.display = "none";
+    document.getElementById("digital_marketing").style.display = "none";
+    document.getElementById("graphic_designing").style.display = "flex";
+  });
