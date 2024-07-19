6c2f06bc02a1e38f7998b9afe781ec1f545a43fb
Harsh Malik <saabharsh78@gmail.com>
2024-07-19 15:04:44 +0530

    done

--- js/home.js
+++ js/home.js
@@ -1,13 +1,13 @@
 document.getElementById("digital_marketing").style.display = "none";
 document.getElementById("graphic_designing").style.display = "none";
-document.getElementById("website_list").style.backgroundColor="#5e45f8"
+document.getElementById("website_list").style.backgroundColor="#cfff04"
 
 
 // WEBISTE 
 document.getElementById("website_list").addEventListener("click", () => {
   document.getElementById("website").style.display = "flex";
   document.getElementById("website").set
-  document.getElementById("website_list").style.backgroundColor="#5e45f8"
+  document.getElementById("website_list").style.backgroundColor="#cfff04"
   document.getElementById("digital_marketing_list").style.backgroundColor="transparent"
   document.getElementById("graphic_designing_list").style.backgroundColor="transparent"
   document.getElementById("digital_marketing").style.display = "none";
@@ -19,7 +19,7 @@ document
   .getElementById("digital_marketing_list")
   .addEventListener("click", () => {
   document.getElementById("website_list").style.backgroundColor="transparent";
-  document.getElementById("digital_marketing_list").style.backgroundColor="#5e45f8";
+  document.getElementById("digital_marketing_list").style.backgroundColor="#cfff04";
   document.getElementById("graphic_designing_list").style.backgroundColor="transparent";
     document.getElementById("website").style.display = "none";
     document.getElementById("digital_marketing").style.display = "flex";
@@ -32,7 +32,7 @@ document
   .addEventListener("click", () => {
     document.getElementById("website_list").style.backgroundColor="transparent";
   document.getElementById("digital_marketing_list").style.backgroundColor="transparent";
-  document.getElementById("graphic_designing_list").style.backgroundColor="#5e45f8";
+  document.getElementById("graphic_designing_list").style.backgroundColor="#cfff04";
     document.getElementById("website").style.display = "none";
     document.getElementById("digital_marketing").style.display = "none";
     document.getElementById("graphic_designing").style.display = "flex";
